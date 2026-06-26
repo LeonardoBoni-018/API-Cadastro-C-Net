1a4be3e7321f86e39f861dd7325cf29cb76a46c5
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-06-26 00:26:42 -0300

    Make modifications infra

--- PedidoApi/Program.cs
+++ PedidoApi/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PedidoApi.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
