18e59c334687efef3ce19422e46db25c9b9daf9c
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-06-26 00:25:54 -0300

    Make user service

---
+++ PedidoApi/Application/Services/UserService.cs
@@ -0,0 +1,9 @@
+public class UserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
