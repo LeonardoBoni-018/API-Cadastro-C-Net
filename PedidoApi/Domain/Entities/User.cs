3812fc8ffc6cb867caf2af8f8f06fe7bfe9e7673
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-06-29 11:10:11 -0300

    Ajust in required values on entity

--- PedidoApi/Domain/Entities/User.cs
+++ PedidoApi/Domain/Entities/User.cs
@@ -1,7 +1,14 @@
 public class User
 {
-    public int Id {get; set;}
-    public string Name {get; set;}
+    public int Id { get; private set; }
 
-    public string Email {get; set;}
+    public string Name { get; private set; }
+
+    public string Email { get; private set; }
+
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
 }
