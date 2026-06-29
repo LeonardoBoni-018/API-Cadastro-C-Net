0aac4f6548daf5be50fdad33b4500c2ec811271d
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-06-29 11:14:47 -0300

    Modify user service

--- PedidoApi/Application/Services/UserService.cs
+++ PedidoApi/Application/Services/UserService.cs
@@ -1,9 +1,24 @@
+// using PedidoApi.Domain.Repositories;
+
+namespace PedidoApi.Application.Services;
+
 public class UserService
 {
-    private readonly IUserRepository _userRepository;
+    private readonly IUserRepository _repository;
 
-    public UserService(IUserRepository userRepository)
+    public UserService(IUserRepository repository)
     {
-        _userRepository = userRepository;
+        _repository = repository;
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<User> CreateAsync(string name, string email)
+    {
+        var user = new User(name, email);
+        return await _repository.SaveAsync(user);
     }
 }
